5440c6c1c5e8d0f3ec4d51c86480ab8b97010c1a
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 21:17:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,4 +17,10 @@ def batch_badge_creator(attendees)
   end
   sentences
 end
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
 
