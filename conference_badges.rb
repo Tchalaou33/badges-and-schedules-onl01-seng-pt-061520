545b63f4dd72c84c961d69692bf390518cc83a10
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 21:17:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,16 @@
  end
 
 
- def batch_badge_creator(attendees)
-   attendees= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-   batch_badge_creator.each do |attendees|
-     puts "Hello, my name is #{attendees}"
- end
+# def batch_badge_creator(attendees)
+#   attendees= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+#   batch_badge_creator.each do |attendees|
+#     puts "Hello, my name is #{attendees}"
+# end
 
+def batch_badge_creator(attendees)
+  sentences = []
+  attendees.each do |attendee|
+    sentences << "Hello, my name is #{attendee}."
+  end
+  sentences
+end
